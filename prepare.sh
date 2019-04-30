d5dbcaa4866c058a1e65de1510818a3a75310ac3
Ronald <ronald.ham@gmail.com>
2019-04-30 22:26:06 +0200

    Update prepare.sh

--- prepare.sh
+++ prepare.sh
@@ -3,6 +3,7 @@
 # create nfs mount
 mkdir -p /mnt/nfs/nfsdlo/$STACK_NETWORK/$STACK_SERVICE-$STACK_VERSION/conf
 mkdir -p /mnt/nfs/nfsdlo/$STACK_NETWORK/$STACK_SERVICE-$STACK_VERSION/data
+mkdir -p /mnt/nfs/nfsdlo/$STACK_NETWORK/$STACK_SERVICE-$STACK_VERSION/redis
 
 
 # create secrets for database
