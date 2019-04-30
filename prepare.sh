efecaa612e6a69c3be7e74d763be6577f617571f
Ronald <ronald.ham@gmail.com>
2019-05-01 00:15:44 +0200

    Update prepare.sh

--- prepare.sh
+++ prepare.sh
@@ -9,3 +9,21 @@ mkdir -p /mnt/nfs/nfsdlo/$STACK_NETWORK/$STACK_SERVICE-$STACK_VERSION/redis
 # create secrets for database
 # alternative date |md5sum|awk '{print $1}' | docker secret create my_secret -
 cat /dev/urandom | tr -dc 'a-zA-Z0-9' | fold -w 32 | head -n 1 | docker secret create canvas_db_root_password -
+
+# create configs for canvas
+docker config create canvas_cache deploy/cache.yml
+docker config create canvas_database deploy/database.yml
+docker config create canvas_delayed_jobs deploy/delayed_jobs.yml
+docker config create canvas_dev_loc deploy/development-local.rb
+docker config create canvas_domain deploy/domain.yml
+docker config create canvas_outgoing_mail deploy/outgoing_mail.yml
+docker config create canvas_redis deploy/redis.yml
+docker config create canvas_security deploy/security.yml
+
+
+
+
+
+
+
+
