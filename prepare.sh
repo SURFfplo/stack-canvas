f5b2e58ecb7b589cc7df3390f7b28ad24dd5bfbf
Ronald <ronald.ham@gmail.com>
2019-04-30 18:07:41 +0200

    Update prepare.sh

--- prepare.sh
+++ prepare.sh
@@ -3,3 +3,7 @@
 # create nfs mount
 mkdir -p /mnt/nfs/nfsdlo/$STACK_NETWORK/$STACK_SERVICE-$STACK_VERSION/conf
 mkdir -p /mnt/nfs/nfsdlo/$STACK_NETWORK/$STACK_SERVICE-$STACK_VERSION/data
+
+
+# create secrets for database
+cat /dev/urandom | tr -dc 'a-zA-Z0-9' | fold -w 32 | head -n 1 | docker secret create canvas_db_root_password -
