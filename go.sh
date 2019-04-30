2c75fa926ceb5e4955805486c550e32507d9ab60
Ronald <ronald.ham@gmail.com>
2019-04-30 16:54:55 +0200

    Create go.sh

---
+++ go.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+export STACK_NETWORK=dev-net
+export STACK_SERVICE=canvas
+export STACK_VERSION=0.1
+
+# prepare
+./prepare.sh
+
+# go
+docker stack deploy -c docker-compose.yml $STACK_SERVICE
