1c7639f527daca76a5d77d12feac83eadd8b3427
hamrt <ronald.ham@gmail.com>
2019-07-08 10:43:47 +0200

    update to allow for automatic key handling

---
+++ create_login.sh
@@ -0,0 +1,26 @@
+#!/usr/bin/env bash
+
+# test mail
+# download swaks
+#curl http://www.jetmore.org/john/code/swaks/files/swaks-20181104.0.tar.gz --output ./swaks-20181104.0.tar.gz
+
+# get api token
+if [[ -v CANVAS_SECRET_API_FILE ]]; then
+        api_key=$(cat ${CANVAS_SECRET_API_FILE})
+        echo $api_key >&2
+else 
+	api_key='123'
+        echo $api_key >&2
+fi
+
+# Create SAML config
+curl 'https://$CANVAS_DOMAIN/api/v1/accounts/1/authentication_providers' \
+     -F 'auth_type=saml' \
+     -F 'idp_entity_id=https://idp.dev.dlo.surf.nl/saml2/idp/metadata.php' \
+     -F 'log_in_url=https://idp.dev.dlo.surf.nl/saml2/idp/SSOService.php' \
+     -F 'log_out_url=https://idp.dev.dlo.surf.nl/saml2/idp/SingleLogoutService.php' \
+     -F 'identifier_format=urn:oasis:names:tc:SAML:2.0:nameid-format:transient' \
+     -F 'login_attribute=mail'
+     -F 'sig_alg=RSA-SHA256' \
+     -F 'certificate_fingerprint=d9:bd:30:11:e7:1d:12:fa:92:e9:3f:95:d6:c4:24:b5:cd:d3:6f:af' \
+     -H 'Authorization: Bearer $api_key'
--- startup.sh
+++ startup.sh
@@ -8,20 +8,50 @@
 #CANVAS_SECRET_FILE= /var/run/secrets/canvas_secret
 
 if [[ -v CANVAS_SECRET_FILE ]]; then
-        key=$(cat ${CANVAS_SECRET_FILE})
+        api_key=$(cat ${CANVAS_SECRET_API_FILE})
         echo $key >&2
-        output=$(echo -n "docker-canvas" | openssl sha1 -hmac $key)
-        devkey=${output#*= }
-        echo $devkey >&2
 else 
-	devkey='123'
-        echo $devkey >&2
+	api_key='123'
+        echo $key >&2
 fi
 
-#key='$(< "${!CANVAS_SECRET_FILE}")'
-#devkey=echo -n "docker-canvas" | openssl sha1 -hmac $key
 
-psql -U $DB_USERNAME -d $DB_NAME -h $DB_HOST -c "INSERT INTO developer_keys (api_key, email, name, redirect_uri) VALUES ('general_developer_key', 'ronald.ham@surfnet.nl', 'Canvas API 4 $CANVAS_DOMAIN', '$CANVAS_DOMAIN');"
+secret=$(grep -A1 'production:' ./config/security.yml | tail -n1); secret=${secret//*encryption_key: /}; echo "$secret"
+
+psql -U $DB_USERNAME -d $DB_NAME -h $DB_HOST -c "delete from developer_keys;"
+psql -U $DB_USERNAME -d $DB_NAME -h $DB_HOST -c "delete from access_tokens;"
+psql -U $DB_USERNAME -d $DB_NAME -h $DB_HOST -c "delete from developer_key_account_bindings;"
+
+
+api_key=$(cat /dev/urandom | tr -dc 'a-zA-Z0-9' | fold -w 64 | head -n 1)
+developer_api_key=$(cat /dev/urandom | tr -dc 'a-zA-Z0-9' | fold -w 64 | head -n 1)
+
+#api_key='$(< "${!CANVAS_SECRET_FILE}")'
+#secret need script to read from secret.yml
+#secret=facdd3a131ddd8988b14f6e4e01039c93cfa0160
+#echo $api_key
+#api_key=SJaVS9t31Y1wHjtvPjsskQ1ScrzP1iQNae6RASmBB9tTngIHSQitpOEfysBEtcxC
+
+crypt=$(echo -n $api_key | openssl sha1 -hmac $secret -binary | xxd -p)
+echo $crypt
+
+psql -U $DB_USERNAME -d $DB_NAME -h $DB_HOST -c "INSERT INTO developer_keys (api_key, email, created_at, name, redirect_uri) VALUES ('$developer_api_key' , 'ronald.ham@surfnet.nl', now(),  'Canvas API 4 $CANVAS_DOMAIN', '$CANVAS_DOMAIN');"
 
 # 'crypted_token' value is hmac sha1 of 'canvas-docker' using default config/security.yml encryption_key value as secret
-psql -U $DB_USERNAME -d $DB_NAME -h $DB_HOST -c "INSERT INTO access_tokens (created_at, crypted_token, developer_key_id, purpose, token_hint, updated_at, user_id) SELECT now(), $devkey, dk.id, 'general_developer_key', '', now(), 1 FROM developer_keys dk where dk.email = 'ronald.ham@surfnet.nl';"
+psql -U $DB_USERNAME -d $DB_NAME -h $DB_HOST -c "INSERT INTO access_tokens (created_at, crypted_token, developer_key_id, purpose, token_hint, updated_at, user_id) SELECT now(), '$crypt', dk.id, 'general_developer_key', '', now(), 1 FROM developer_keys dk where dk.email = 'ronald.ham@surfnet.nl';"
+
+# need to activate the token 
+psql -U $DB_USERNAME -d $DB_NAME -h $DB_HOST -c "update developer_key_account_bindings set workflow_state = 'on' where account_id = 2 and developer_key_id = (select id FROM developer_keys dk where dk.email = 'ronald.ham@surfnet.nl');"
+
+
+#psql -U $DB_USERNAME -d $DB_NAME -h $DB_HOST -c "SELECT * FROM access_tokens;"
+#psql -U $DB_USERNAME -d $DB_NAME -h $DB_HOST -c "SELECT * FROM developer_keys;"
+#psql -U $DB_USERNAME -d $DB_NAME -h $DB_HOST -c "SELECT * FROM developer_key_account_bindings;"
+
+
+
+#info in canvas for creating key
+#https://github.com/instructure/canvas-lms/blob/88fae607ae7c386912992062990a6c405d6d76ab/lib/canvas/security.rb
+#https://github.com/instructure/canvas-lms/blob/a664cdb0b26bf9d4473c0204dba38fc73a34ece7/lib/canvas/security/services_jwt.rb
+
+
