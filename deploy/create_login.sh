f5372d942c740854a4dbc8256766d047c6622234
Herman <herman@dompseler.nl>
2019-07-11 16:46:57 +0200

    retry idp environment with different names

--- deploy/create_login.sh
+++ deploy/create_login.sh
@@ -20,11 +20,11 @@ URL="http://localhost/api/v1/accounts/1/authentication_providers"
 curl -s "$URL" \
      -F 'auth_type=saml' \
      -F 'position=1' \
-     -F "idp_entity_id=${STACK_NETWORK_URL_IDP}/saml2/idp/metadata.php" \
-     -F "log_in_url=${STACK_NETWORK_URL_IDP}/saml2/idp/SSOService.php" \
-     -F "log_out_url=${STACK_NETWORK_URL_IDP}/saml2/idp/SingleLogoutService.php" \
+     -F "idp_entity_id=${IDP_URL}/saml2/idp/metadata.php" \
+     -F "log_in_url=${IDP_URL}/saml2/idp/SSOService.php" \
+     -F "log_out_url=${IDP_URL}/saml2/idp/SingleLogoutService.php" \
      -F 'identifier_format=urn:oasis:names:tc:SAML:2.0:nameid-format:transient' \
      -F 'login_attribute=mail' \
      -F 'sig_alg=RSA-SHA256' \
-     -F "certificate_fingerprint=${STACK_NETWORK_IDP_FINGERPRINT}" \
+     -F "certificate_fingerprint=${IDP_FINGERPRINT}" \
      -H "Authorization: Bearer $api_key"
