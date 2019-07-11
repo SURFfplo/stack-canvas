9c3de87c861057e99031596d94b4af4f53a74575
Herman <herman@dompseler.nl>
2019-07-11 15:17:56 +0200

    ook canvas voor test en exp

--- deploy/create_login.sh
+++ deploy/create_login.sh
@@ -20,11 +20,11 @@ URL="http://localhost/api/v1/accounts/1/authentication_providers"
 curl -s "$URL" \
      -F 'auth_type=saml' \
      -F 'position=1' \
-     -F 'idp_entity_id=https://idp.dev.dlo.surf.nl/saml2/idp/metadata.php' \
-     -F 'log_in_url=https://idp.dev.dlo.surf.nl/saml2/idp/SSOService.php' \
-     -F 'log_out_url=https://idp.dev.dlo.surf.nl/saml2/idp/SingleLogoutService.php' \
+     -F "idp_entity_id=${STACK_NETWORK_URL_IDP}/saml2/idp/metadata.php" \
+     -F "log_in_url=${STACK_NETWORK_URL_IDP}/saml2/idp/SSOService.php" \
+     -F "log_out_url=${STACK_NETWORK_URL_IDP}/saml2/idp/SingleLogoutService.php" \
      -F 'identifier_format=urn:oasis:names:tc:SAML:2.0:nameid-format:transient' \
      -F 'login_attribute=mail' \
      -F 'sig_alg=RSA-SHA256' \
-     -F 'certificate_fingerprint=d9:bd:30:11:e7:1d:12:fa:92:e9:3f:95:d6:c4:24:b5:cd:d3:6f:af' \
+     -F "certificate_fingerprint=${STACK_NETWORK_IDP_FINGERPRINT}" \
      -H "Authorization: Bearer $api_key"
