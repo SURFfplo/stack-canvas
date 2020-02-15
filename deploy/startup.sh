8a96b7346c287d9749e6a7b48ae5dbc2b8f5a72a
Ronald <ronald.ham@gmail.com>
2020-02-15 18:50:13 +0100

    Update startup.sh

--- deploy/startup.sh
+++ deploy/startup.sh
@@ -37,26 +37,25 @@ secret=$(cat $CANVAS_SECRET_FILE)
 #echo $api_key
 #api_key=SJaVS9t31Y1wHjtvPjsskQ1ScrzP1iQNae6RASmBB9tTngIHSQitpOEfysBEtcxC
 
-crypt=$(echo -n $api_key | openssl sha1 -hmac $secret -binary | xxd -p)
+crypt=$(ruby ./config/create_crypted_token.rb)
+#crypt=$(echo -n $api_key | openssl sha1 -hmac $secret -binary | xxd -p)
 echo $crypt
 
 #psql -U $DB_USERNAME -d $DB_NAME -h $DB_HOST -c "INSERT INTO developer_keys (api_key, email, created_at, name, redirect_uri) VALUES ('$developer_api_key' , 'ronald.ham@surfnet.nl', now(),  'Canvas API 4 $CANVAS_DOMAIN', '$CANVAS_DOMAIN');"
 
-#psql -U $DB_USERNAME -d $DB_NAME -h $DB_HOST -c "INSERT INTO developer_keys (api_key, email, created_at, name, redirect_uri) VALUES ('$api_key' , 'ronald.ham@surfnet.nl', now(),  'Canvas API 4 $CANVAS_DOMAIN', 'https://$CANVAS_DOMAIN');"
+psql -U $DB_USERNAME -d $DB_NAME -h $DB_HOST -c "INSERT INTO developer_keys (api_key, email, created_at, name, redirect_uri) VALUES ('$api_key' , 'ronald.ham@surfnet.nl', now(),  'Canvas API 4 $CANVAS_DOMAIN', 'https://$CANVAS_DOMAIN');"
 
 # 'crypted_token' value is hmac sha1 of 'canvas-docker' using default config/security.yml encryption_key value as secret
-#psql -U $DB_USERNAME -d $DB_NAME -h $DB_HOST -c "INSERT INTO access_tokens (created_at, crypted_token, developer_key_id, purpose, token_hint, updated_at, user_id) SELECT now(), '$crypt', dk.id, 'general_developer_key', '', now(), 1 FROM developer_keys dk where dk.email = 'ronald.ham@surfnet.nl';"
+psql -U $DB_USERNAME -d $DB_NAME -h $DB_HOST -c "INSERT INTO access_tokens (created_at, crypted_token, developer_key_id, purpose, token_hint, updated_at, user_id) SELECT now(), '$crypt', dk.id, 'general_developer_key', '', now(), 1 FROM developer_keys dk where dk.email = 'ronald.ham@surfnet.nl';"
 
 sleep 10
 # need to activate the token 
-#psql -U $DB_USERNAME -d $DB_NAME -h $DB_HOST -c "update developer_key_account_bindings set workflow_state = 'on' where account_id = 2 and developer_key_id = (select id FROM developer_keys dk where dk.email = 'ronald.ham@surfnet.nl');"
+psql -U $DB_USERNAME -d $DB_NAME -h $DB_HOST -c "update developer_key_account_bindings set workflow_state = 'on' where account_id = 2 and developer_key_id = (select id FROM developer_keys dk where dk.email = 'ronald.ham@surfnet.nl');"
 
 
 #psql -U $DB_USERNAME -d $DB_NAME -h $DB_HOST -c "SELECT * FROM access_tokens;"
 #psql -U $DB_USERNAME -d $DB_NAME -h $DB_HOST -c "SELECT * FROM developer_keys;"
 #psql -U $DB_USERNAME -d $DB_NAME -h $DB_HOST -c "SELECT * FROM developer_key_account_bindings;"
-
-
 
 #info in canvas for creating key
 #https://github.com/instructure/canvas-lms/blob/88fae607ae7c386912992062990a6c405d6d76ab/lib/canvas/security.rb
