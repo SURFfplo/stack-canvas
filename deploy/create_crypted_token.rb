f9cc93c239cc75e87a01ff9475a415fbbc1e05a0
Ronald <ronald.ham@gmail.com>
2020-02-16 01:52:37 +0100

    Update create_crypted_token.rb
    add chomp to remove trailing /n from file input

--- deploy/create_crypted_token.rb
+++ deploy/create_crypted_token.rb
@@ -1,7 +1,7 @@
 require "openssl"
 secret_file=ENV["CANVAS_SECRET_FILE"]
 api_key_file=ENV["CANVAS_API_KEY_FILE"]
-secret = File.read(secret_file)
-api_key = File.read(api_key_file)
+secret = File.read(secret_file).chomp
+api_key = File.read(api_key_file).chomp
 outputval=OpenSSL::HMAC.hexdigest( OpenSSL::Digest.new('sha1'), secret, api_key)
 $stdout.write outputval
