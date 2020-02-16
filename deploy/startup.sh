0b029c9b240f56df9cdd815b4a786e6909019fc4
Ronald <ronald.ham@gmail.com>
2020-02-17 00:25:06 +0100

    Update startup.sh

--- deploy/startup.sh
+++ deploy/startup.sh
@@ -14,14 +14,14 @@ crypt=$(ruby ./config/create_crypted_token.rb)
 
 #psql -U $DB_USERNAME -d $DB_NAME -h $DB_HOST -c "INSERT INTO developer_keys (api_key, email, created_at, name, redirect_uri) VALUES ('$api_key' , 'ronald.ham@surfnet.nl', now(),  'Canvas API 4 $CANVAS_DOMAIN', '$CANVAS_DOMAIN');"
 
-psql -U $DB_USERNAME -d $DB_NAME -h $DB_HOST -c "INSERT INTO developer_keys (api_key, email, created_at, name, redirect_uri) VALUES ('$developer_key' , '$STACK_ADMIN_EMAIL', now(),  'Canvas API 4 $CANVAS_DOMAIN', 'https://$CANVAS_DOMAIN');"
+psql -U $DB_USERNAME -d $DB_NAME -h $DB_HOST -c "INSERT INTO developer_keys (api_key, email, created_at, name, redirect_uri) VALUES ('$developer_key' , '$CANVAS_LMS_ADMIN_EMAIL', now(),  'Canvas API 4 $CANVAS_DOMAIN', 'https://$CANVAS_DOMAIN');"
 
 # 'crypted_token' value is hmac sha1 of 'canvas-docker' using default config/security.yml encryption_key value as secret
-psql -U $DB_USERNAME -d $DB_NAME -h $DB_HOST -c "INSERT INTO access_tokens (created_at, crypted_token, developer_key_id, purpose, token_hint, updated_at, user_id) SELECT now(), '$crypt', dk.id, 'general_developer_key', '', now(), 1 FROM developer_keys dk where dk.email = '$STACK_ADMIN_EMAIL';"
+psql -U $DB_USERNAME -d $DB_NAME -h $DB_HOST -c "INSERT INTO access_tokens (created_at, crypted_token, developer_key_id, purpose, token_hint, updated_at, user_id) SELECT now(), '$crypt', dk.id, 'general_developer_key', '', now(), 1 FROM developer_keys dk where dk.email = '$CANVAS_LMS_ADMIN_EMAIL';"
 
 sleep 10
 # need to activate the token 
-psql -U $DB_USERNAME -d $DB_NAME -h $DB_HOST -c "update developer_key_account_bindings set workflow_state = 'on' where account_id = 2 and developer_key_id = (select id FROM developer_keys dk where dk.email = '$STACK_ADMIN_EMAIL');"
+psql -U $DB_USERNAME -d $DB_NAME -h $DB_HOST -c "update developer_key_account_bindings set workflow_state = 'on' where account_id = 2 and developer_key_id = (select id FROM developer_keys dk where dk.email = '$CANVAS_LMS_ADMIN_EMAIL');"
 
 
 #psql -U $DB_USERNAME -d $DB_NAME -h $DB_HOST -c "SELECT * FROM access_tokens;"
